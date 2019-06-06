cab41efef91af4f53c01bd5026bd3fed49ab73cd
AyushmaM <ayushmamalla16@gmail.com>
2019-06-06 07:03:52 +0000

    placecomment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,16 @@
 class CommentsController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
 
-	def 
-		@place = Place.find(params[:place_id])
-		@place.comments.create(.merge(user: current_user))
-		redirect_to place_path(@place)
-	end
+  def create
+    @place = Place.find(params[:place_id])
+   @place.comments.create(comment_params.merge(user: current_user))
+    redirect_to place_path(@place)
+  end
 
-	private
+  private
 
-	def comment_params
-		params.require(:comment).permit(:message, :rating)
-	end
+  def comment_params
+    params.require(:comment).permit(:message, :rating)
+  end
 end
 
-<h1>Comments</h1>
-
-<% @place.comments.each do |comment| %>
-	<div>
-	<blockquote>
-	<b><%= comment.rating %></b> -
-	<%= comment.message %>
-	<small><%= comment.user.email %></small>
-	</blockquote>
-	</div>
-	<% end %>
