4045081cc60ae21741b8403278de3da02afc39bc
AyushmaM <ayushmamalla16@gmail.com>
2019-06-06 06:36:40 +0000

    form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,10 +18,10 @@ class PlacesController < ApplicationController
     end
   end
 
-  def show
-    @place = Place.find(params[:id])
-    @comment = Comment.new
-  end
+ def show
+  @place = Place.find(params[:id])
+  @comment = Comment.new
+end
 
   def edit
     @place = Place.find(params[:id])
