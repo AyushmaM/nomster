7a92e84d2e56d15f02a52707772aeac90cf39dae
AyushmaM <ayushmamalla16@gmail.com>
2019-06-18 03:59:50 +0000

    fix

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,9 @@
 class Place < ApplicationRecord
 	 belongs_to :user
-  has_many :comments
+	 has_many :comments
   
-  geocoded_by :address
-  after_validation :geocode
+	  geocoded_by :address
+	  after_validation :geocode
 	
 	validates :name, presence: true, length: {minimum: 3}
 	validates :address, :description, presence: true
