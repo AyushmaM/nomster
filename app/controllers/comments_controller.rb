9bc9d05a083dca7d04e57e7e10d07a56e1348b43
AyushmaM <ayushmamalla16@gmail.com>
2019-06-20 17:38:55 +0000

    counting_commits

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
-class CommentsController < ApplicationController
+    class CommentsController < ApplicationController
   before_action :authenticate_user!
+  
 
   def create
     @place = Place.find(params[:place_id])
-   @place.comments.create(comment_params.merge(user: current_user))
+    @place.comments.create(comment_params.merge(user: current_user)) 
     redirect_to place_path(@place)
   end
 
@@ -13,4 +14,3 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:message, :rating)
   end
 end
-
