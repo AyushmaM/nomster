59b9224c404b4992e8f0fd18e300172a76748965
AyushmaM <ayushmamalla16@gmail.com>
2019-06-20 00:54:41 +0000

    email

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,13 +10,13 @@ class PlacesController < ApplicationController
   end
 
   def create
-    @place = current_user.places.create(place_params)
-    if @place.valid?
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
+  @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
  def show
   @place = Place.find(params[:id])
@@ -32,18 +32,19 @@ def edit
   end
 
   def update
-    @place = Place.find(params[:id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
+  @place = Place.find(params[:id])
 
-    @place.update_attributes(place_params)
-    if @place.valid?
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
+  @place.update_attributes(place_params)
+  if @place.valid?
     redirect_to root_path
   else
     render :edit, status: :unprocessable_entity
   end
-  end
+end
 
   def destroy
     @place = Place.find(params[:id])
